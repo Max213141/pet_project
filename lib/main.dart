c637f3b72fc2c98818a860f07a96bb067b95d3ec
Купцов Максим Демьянович <maksim.kuptsou@softclub.by>
2024-04-29 18:35:25 +0300

    Added splash screen from lib, but i still need to use my SplasScreen while launch handles

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:life_sync/my_app.dart';
 import 'package:life_sync/utils/utils.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'firebase_options.dart';
 
@@ -67,7 +68,8 @@ void main() async {
   //   sound: true,
   // );
   // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
     MyApp(
       auth: auth,
